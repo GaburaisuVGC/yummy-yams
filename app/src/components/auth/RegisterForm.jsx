6692718dd96863b274e4e859409068d6d57e2840
gaburaisu <56201393+GaburaisuVGC@users.noreply.github.com>
2024-05-15 10:05:36 +0200

    fix: all buttons must be in french

--- app/src/components/auth/RegisterForm.jsx
+++ app/src/components/auth/RegisterForm.jsx
@@ -22,7 +22,7 @@ const RegisterForm = () => {
     <form onSubmit={handleSubmit}>
       <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
       <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit">Register</button>
+      <button type="submit">S'inscrire</button>
     </form>
   );
 };
